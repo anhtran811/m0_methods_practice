abbe67ce716a50eedc053c6f03c74004f5f92e29
Anhnhi Tran <anhtran811@gmail.com>
2022-08-30 19:27:35 -0600

    Added definitions

--- define_and_id.rb
+++ define_and_id.rb
@@ -11,17 +11,27 @@
 
 
 "Hello World".include?("Hello")
-
+# the include? method is called on the string object "Hello World"
+# the argument "Hello" is being passed to the include? method
+# include? returns the value true if the object "Hello" is present
+# the return value is true
 
 "Hello World".end_with?("Hello")
-
+# the end_with? method is called on the string object "Hello World"
+# the argument "Hello" is being passed to the end_with? method
+# end_with returns the value true if the string object "Hello World" ends with the suffix given "Hello"
+#the return value is false
 
 "Hello World".end_with?("rld")
-
+# the end_with? method is called on the string object "Hello World"
+# the argument "rld" is being passed to the end_with? method
+# end_with returns the value true if the string object "Hello World" ends with the suffix given "rld"
+#the return value is true
 
 12.even?
-
+# the even? method returns a boolean value (true or false)
+# it will return a value of true if the number is even
+# no arguments are passed
 
 18.next
-
-
+# the next method allows you to skip the execution of an iteration 
